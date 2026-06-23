fbad039dd16416b74572d2118860f1e0ea34fd44
bastianolea <bastianolea@gmail.com>
2026-06-22 22:31:38 -0400

    clase 1

--- clases/clase_1.R
+++ clases/clase_1.R
@@ -1,116 +1,90 @@
-# Clase 1
-# Introducción al análisis de datos con R para ciencias sociales
-# 25-03-2026
-# Bastián Olea Herrera - https://bastianolea.rbind.io - bastianolea@gmail.com
+# esta es la primera clase
 
-# diapositivas: https://bastianolea.github.io/curso_intro_R_gratis_v2/
+# 5 + perro
 
-# en el script podemos ejecutar las lineas con control + enter
-1+2
-6-5 # ejemplo de resta
+# cargar los datos que vienen en excel
 
-# operaciones matemáticas ----
-
-# sumar dos números
-1+1
-
-# sumar muchos números en una sola línea
-4+4+5+5+5+5+5+5+5+5 
-
-# multiplicar números grandes
-89889*65546
-
-# podemos comentar líneas de código para que no se ejecuten
-# 34343-65656+3232*4343
+687676 + 3435 # una suma absurda
 
 
-## trabajar con texto ----
+"ésta es una cadena de texto"
 
-# en R, el texto se escribe entre comillas
-"gato"
+"perro"
 
-# no se pueden sumar textos como si fueran números
-# "gato" + "perro" # esto genera un error
+" "
+34 * 4545
 
-# tampoco se pueden sumar números con texto!
-# 5 + "5"
+2 + 2
+# 
+# "2" + "2"
 
-
-
-
-# asignación de variables ----
-# guardar un valor en una variable usando <-
-
-# guardar el año actual en una variable
-año <- 2026
-# notemos que al crear un objeto éste aparece en el panel de entorno (Environment)
-
-# ver el contenido de la variable
-año
-
-# guardar un texto en una variable
-nombre <- "basti"
+# "gato" + "perro"
 
 
-# reasignar valores ----
+# creación de un objeto nuevo
+animal <- "gato"
 
-# podemos cambiar el valor de una variable existente
-año <- 1993
+animal
 
+edad <- 43
 
-## operaciones con variables ----
+edad - 2026
+2026 - edad
 
-# crear dos variables con números
-edad <- 32
-año <- 2026
+año <- 2029
 
-# restar variables (el orden importa)
-edad - año
 año - edad
 
+resultado_resta <- año - edad
 
-# ejemplo de una operación un poco más compleja con objetos
-presupuesto = 100000
-pizza = 15000
-presupuesto - (pizza * 4)
+resultado_resta
+
+c(1, 2, 3)
 
 
-# instalación de paquetes ----
-# los paquetes nos permiten extender R con funcionalidades nuevas
+edades <- c(33, 44, 52, 32, 58, 23, 64, 38, 
+            # estas son mas edades
+            33, 44, 52, 32, 58, 23, 64, 38, # lalalal 
+            33, 44, 52, 32, 58, 23, 64, 38, 
+            # y estas son las últimas
+            33, 44, 52, 32, 58, 23, 64, 38,
+            43,
+            54, 78)
 
-# la instalación sólo es necesaria de hacer una vez, ya que es como bajar una app
+edad_maxima <- max(edades)
 
-# instalamos paquete para cargar datos desde excel
-install.packages("readxl")
+edad_maxima
 
-# luego cargamos el paquete, que es como abrir una app
-library(readxl) 
+paste("la edad máxima es:",
+      edad_maxima,
+      "años")
 
-# ahora usamos una función del paquete para cargar datos
-# las funciones son como pequeños programas que realizan acciones
-# en este caso, la función "read_excel" carga un archivo si le das la ruta al mismo
 
-datos <- read_excel("estimaciones_pobreza.xlsx")
-# creamos un objeto "datos" que contiene los datos del archivo
+edades - año
+
+
+mean(edades)
+
+median(edades)
+
+sum(edades)
+
+max(edades)
+
+sum(
+  c(44534543, 
+    345345, 
+    345345)
+)
+
+
+
+
+# install.packages("readxl")
+
+library("readxl")
+
+datos <- read_xlsx("estimaciones_pobreza.xlsx")
+
 
 datos
-
-
-# ahora instalamos {dplyr}, paquete para trabajar con datos
-install.packages("dplyr")
-
-# cargamos el paquete
-library(dplyr)
-
-# nos entrega funciones para explorar datos
-glimpse(datos)
-
-# seleccionar columnas
-datos |> select(comuna, region)
-# esto significa: "al objeto 'datos' luego le selecciono las columnas comuna y region"
-
-# seleccionar y ordenar columnas
-datos |> 
-  select(region, comuna, personas) |> 
-  arrange(personas)
-# "al objeto 'datos' luego le selecciono 3 columnas y ordeno los datos según la variable personas"
